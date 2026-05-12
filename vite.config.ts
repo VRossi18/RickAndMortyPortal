e1e0cfc53dcf1633be6035f99c8803a07f6d0673
Vinicius Rossi <viniciusprossi18@gmail.com>
2026-05-12 15:39:06 -0300

    deploy on github pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,9 @@ import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
 
+const GITHUB_PAGES_BASE = '/rick-morty-portal/';
+
 export default defineConfig({
+   base: process.env.NODE_ENV === 'production' ? GITHUB_PAGES_BASE : '/',
    plugins: [react(), tailwindcss()],
 });
