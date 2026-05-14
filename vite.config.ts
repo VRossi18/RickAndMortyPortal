dca1de93f936c4e74b44f98ea008a66b18f513fe
Vinicius Rossi <viniciusprossi18@gmail.com>
2026-05-14 08:01:23 -0300

    fixed base url

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
 
-const GITHUB_PAGES_BASE = '/rick-morty-portal/';
+const GITHUB_PAGES_BASE = '/RickAndMortyPortal/';
 
 export default defineConfig({
    base: process.env.NODE_ENV === 'production' ? GITHUB_PAGES_BASE : '/',
